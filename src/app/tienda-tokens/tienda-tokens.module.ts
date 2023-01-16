4f70b3d182e4a85c94b6bc39ea8e30b398cf3be8
Joaquin Lucero <jlucero@iesfuentesanluis.org>
2023-01-16 11:37:37 +0100

    Anyadido un col, mi parte de CSS de los PopUps y un nuevo modulo y componente

---
+++ src/app/tienda-tokens/tienda-tokens.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MostrarServiciosComponent } from './mostrar-servicios/mostrar-servicios.component';
+
+
+
+@NgModule({
+  declarations: [
+    MostrarServiciosComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TiendaTokensModule { }
