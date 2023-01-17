a58dfd2f74069056c6ad115748a1f2316fe1bf29
Borja Clase <bvalls@iesfuentesanluis.org>
2023-01-17 09:58:22 +0100

    Añadido modulos y otrras cosas

--- src/app/utilidades/cajas-desplegables/cajas-desplegables.component.ts
+++ src/app/utilidades/cajas-desplegables/cajas-desplegables.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cajas-desplegables',
   templateUrl: './cajas-desplegables.component.html',
   styleUrls: ['./cajas-desplegables.component.css']
 })
-export class CajasDesplegablesComponent {
-
+export class CajasDesplegablesComponent{
+  prueba:boolean = true;
+  elemento:HTMLElement = document.getElementById("flechaCaja")!;
+  // @ViewChild("flechaCaja") flechaCaja!: ElementRef;
+  rotar(event: Event){
+    console.log("Entra en evento");
+    if (this.prueba == true) {
+      this.elemento.style.transform="rotate(90deg)";
+      this.prueba = false;
+      console.log("Entra");
+    } else if(this.prueba == false){
+      this.elemento.style.transform="rotate(none)";
+      this.prueba = true;
+      console.log("Sale");
+    }
+  }
 }
