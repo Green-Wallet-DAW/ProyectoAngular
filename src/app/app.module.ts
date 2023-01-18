2a98ebf3d2cf61d235b69fbee3d64023a52fe0a7
Borja Clase <bvalls@iesfuentesanluis.org>
2023-01-18 10:01:32 +0100

    Lista usuarios terminada, cdn de iconos añadido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UtilidadesModule } from './utilidades/utilidades.module';
+import { TablasModule } from "./tablas/tablas.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    UtilidadesModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        UtilidadesModule,
+        TablasModule
+    ]
 })
 export class AppModule { }
