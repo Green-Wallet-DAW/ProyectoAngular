fb3ab544faab68f9db2c02df3a9f1858a63b6b76
Borja Clase <bvalls@iesfuentesanluis.org>
2023-01-31 14:34:11 +0100

    retoques

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MostrarServiciosComponent } from './tienda-tokens/mostrar-servicios/mostrar-servicios.component';
 import { UtilidadesModule } from './utilidades/utilidades.module';
-import { FacilityModule } from './facility/facility.module';import { TablasModule } from "./tablas/tablas.module";
+import { FacilityModule } from './facility/facility.module';
+import { TablasModule } from "./tablas/tablas.module";
 import { TiendaTokensModule } from './tienda-tokens/tienda-tokens.module';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
     declarations: [
@@ -16,6 +18,7 @@ import { TiendaTokensModule } from './tienda-tokens/tienda-tokens.module';
     bootstrap: [AppComponent],
     imports: [
         BrowserModule,
+        RouterModule,
         AppRoutingModule,
         UtilidadesModule,
         TablasModule,
