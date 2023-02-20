4908afdc1f1e4ea1073964821a9433f620227643
Borja Clase <bvalls@iesfuentesanluis.org>
2023-02-20 10:04:52 +0100

    confirmacion doble paso

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router';
 import { AuthService } from '../_services/auth.service';
 import { matchValidator } from './form-validator';
 import { ToastrService } from 'ngx-toastr';
+import {callJSFun} from '../register/emails.js';
+
 
 @Component({
   selector: 'app-register',
@@ -22,10 +24,13 @@ export class RegisterComponent implements OnInit {
     cpassword: new FormControl('', [Validators.required, Validators.minLength(10), matchValidator('password')]),
     conf: new FormControl('', [Validators.required]),
   });
+  form2 = new FormGroup({
+    code: new FormControl('', [Validators.required]),
+  });
+  
 
-
-
-  toast1= false;
+  valid = false;
+  code = '';
   isSuccessful = false;
   isSignUpFailed = false;
   errorMessage = '';
@@ -35,18 +40,42 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  stringGenerator(){
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < 10) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+  }
+
   onSubmit(): void {
-    // console.log(this.form);
     if(this.form.valid){
+      this.valid = true;
+      this.code = this.stringGenerator();
+      
+      callJSFun(this.form.value.email, this.form.value.name, this.code);
+
+    }else{
+      this.toastr.error("The form did not validated correctly", 'ERROR CREATING ACCOUNT', {positionClass: 'toast-bottom-right', timeOut:5000});
+    }
+  }
+
+  onSubmit2(){
+    if(this.form2.valid && this.form2.value.code! == this.code){
+      
       this.authService.register(this.form.value.name!, this.form.value.email!, this.form.value.password!, this.form.value.phone_number!, this.form.value.cumn!, this.form.value.newsletter!).subscribe(
         data => {
           this.isSuccessful = true;
           this.isSignUpFailed = false;
           this.toastr.success('Your information has been registered successfully! You will be redirected now to the login', 'Account created', {positionClass: 'toast-bottom-center', timeOut: 5000});
           
+          console.log("redirect?");
           this.router.navigate(['/login']);
 
-          this.toast1 = true;
         },
         error => {
           if(error instanceof ErrorEvent){
@@ -57,8 +86,9 @@ export class RegisterComponent implements OnInit {
           this.isSignUpFailed = true;
         }
       );
+
     }else{
-      this.toastr.error("The form did not validated correctly", 'ERROR CREATING ACCOUNT', {positionClass: 'toast-bottom-right', timeOut:5000});
+      this.toastr.error("The code does not match", 'ERROR CREATING ACCOUNT', {positionClass: 'toast-bottom-right', timeOut:5000});
     }
   }
 }
