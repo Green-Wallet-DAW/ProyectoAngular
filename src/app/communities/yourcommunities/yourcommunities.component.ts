64e8a36b1179548365af73ac4c33580fc7595eb1
croaua <croaua@iesfuentesanluis.org>
2023-02-21 09:36:29 +0100

    instalacion

--- src/app/communities/yourcommunities/yourcommunities.component.ts
+++ src/app/communities/yourcommunities/yourcommunities.component.ts
@@ -21,4 +21,26 @@ export class YourcommunitiesComponent implements OnInit{
   verUsuario(id: string) {
     this.comunidadesService.getUsuarios(id).subscribe();
   }
+  // eliminarEmpleado(id: string) {
+  //   this.comunidadesService.borrarComunidad(id)
+  //     .subscribe(
+  //       result => {
+  //         // console.log("Empleado eliminado con éxito");
+  //         this.toastr.success('Empleado eliminado con éxito!!', 'Eliminado empleado', {positionClass: 'toast-bottom-right', timeOut:2000});
+  //         this.getEmpleados();
+  //       },
+  //       error => {
+  //         this.mensajeErr = '';
+  //         if (error instanceof ErrorEvent) {
+  //           this.mensajeErr = error.error.message;
+  //         }
+  //         else if (error.status == 404) {
+  //           this.mensajeErr = "Error 404"
+  //         } else {
+  //           this.mensajeErr = "Error status:" + error.status;
+  //         }
+  //         console.log(this.mensajeErr);
+  //       }
+  //     );
+  // }
 }
